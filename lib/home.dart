8f58060a7f390b1ad50c1f13efd5d472fd5c4fa7
Danilo Andrade <danilo_as@live.com>
2024-03-31 16:49:35 -0300

    atualização 31032024

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:sementes_ufes/amendoim/questoes.amendoim.1to5.dart';
 import 'package:sementes_ufes/quiz.page.dart';
 import 'package:url_launcher/url_launcher.dart';
 
