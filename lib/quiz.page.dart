4d86b67ab4c2df34d732c202ce24e957f57bbebe
Danilo Andrade <danilo_as@live.com>
2024-04-01 19:32:56 -0300

    atualização 01042024

--- lib/quiz.page.dart
+++ lib/quiz.page.dart
@@ -10,12 +10,12 @@ class QuizPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF49426E),
+      backgroundColor: const Color(0xFF49426E),
       body: Row(
         children: [
           Container(
             width: 60, // Largura da barra lateral
-            color: Color(0xFF322E53),
+            color: const Color(0xFF322E53),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -85,10 +85,11 @@ class QuizPage extends StatelessWidget {
                           scrollDirection: Axis.vertical,
                           children: [
                             Container(
+                              height: 70,
                               margin:
                                   const EdgeInsets.only(left: 20, right: 20),
                               decoration: BoxDecoration(
-                                color: Color(0xFFF1C28E),
+                                color: const Color(0xFFF1C28E),
                                 borderRadius: BorderRadius.circular(10),
                               ),
                               child: TextButton.icon(
@@ -150,10 +151,11 @@ class QuizPage extends StatelessWidget {
                               height: 20,
                             ),
                             Container(
+                              height: 70,
                               margin:
                                   const EdgeInsets.only(left: 20, right: 20),
                               decoration: BoxDecoration(
-                                color: Color(0xFFF1C28E),
+                                color: const Color(0xFFF1C28E),
                                 borderRadius: BorderRadius.circular(10),
                               ),
                               child: TextButton.icon(
@@ -215,10 +217,11 @@ class QuizPage extends StatelessWidget {
                               height: 20,
                             ),
                             Container(
+                              height: 70,
                               margin:
                                   const EdgeInsets.only(left: 20, right: 20),
                               decoration: BoxDecoration(
-                                color: Color(0xFFF1C28E),
+                                color: const Color(0xFFF1C28E),
                                 borderRadius: BorderRadius.circular(10),
                               ),
                               child: TextButton.icon(
@@ -280,10 +283,11 @@ class QuizPage extends StatelessWidget {
                               height: 20,
                             ),
                             Container(
+                              height: 70,
                               margin:
                                   const EdgeInsets.only(left: 20, right: 20),
                               decoration: BoxDecoration(
-                                color: Color(0xFFF1C28E),
+                                color: const Color(0xFFF1C28E),
                                 borderRadius: BorderRadius.circular(10),
                               ),
                               child: TextButton.icon(
@@ -331,7 +335,7 @@ class QuizPage extends StatelessWidget {
                                   minimumSize: const Size(100, 50),
                                 ),
                                 label: const Text(
-                                  "Tecnologia e Produção de Sementes",
+                                  "Tecnologia e Produção de Sementes Sintéticas",
                                   style: TextStyle(
                                     fontFamily: 'Merriweather',
                                     fontSize: 12,
@@ -345,10 +349,11 @@ class QuizPage extends StatelessWidget {
                               height: 20,
                             ),
                             Container(
+                              height: 70,
                               margin:
                                   const EdgeInsets.only(left: 20, right: 20),
                               decoration: BoxDecoration(
-                                color: Color(0xFFF1C28E),
+                                color: const Color(0xFFF1C28E),
                                 borderRadius: BorderRadius.circular(10),
                               ),
                               child: TextButton.icon(
@@ -410,10 +415,11 @@ class QuizPage extends StatelessWidget {
                               height: 20,
                             ),
                             Container(
+                              height: 70,
                               margin:
                                   const EdgeInsets.only(left: 20, right: 20),
                               decoration: BoxDecoration(
-                                color: Color(0xFFF1C28E),
+                                color: const Color(0xFFF1C28E),
                                 borderRadius: BorderRadius.circular(10),
                               ),
                               child: TextButton.icon(
@@ -461,7 +467,7 @@ class QuizPage extends StatelessWidget {
                                   minimumSize: const Size(100, 50),
                                 ),
                                 label: const Text(
-                                  "Produção orgânico de trigo, girassol e amendoim",
+                                  "Produção orgânica de trigo, girassol e amendoim",
                                   style: TextStyle(
                                     fontFamily: 'Merriweather',
                                     fontSize: 12,
@@ -475,10 +481,11 @@ class QuizPage extends StatelessWidget {
                               height: 20,
                             ),
                             Container(
+                              height: 70,
                               margin:
                                   const EdgeInsets.only(left: 20, right: 20),
                               decoration: BoxDecoration(
-                                color: Color(0xFFF1C28E),
+                                color: const Color(0xFFF1C28E),
                                 borderRadius: BorderRadius.circular(10),
                               ),
                               child: TextButton.icon(
@@ -527,6 +534,202 @@ class QuizPage extends StatelessWidget {
                                 ),
                                 label: const Text(
                                   "Hormônios na fisiologia das sementes",
+                                  style: TextStyle(
+                                    fontFamily: 'Merriweather',
+                                    fontSize: 12,
+                                    color: Color(0xFF322E53),
+                                  ),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Container(
+                              height: 70,
+                              margin:
+                                  const EdgeInsets.only(left: 20, right: 20),
+                              decoration: BoxDecoration(
+                                color: const Color(0xFFF1C28E),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: TextButton.icon(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    PageRouteBuilder(
+                                      pageBuilder:
+                                          (context, animation1, animation2) =>
+                                              AmendoimList(),
+                                      transitionsBuilder: (context, animation1,
+                                          animation2, child) {
+                                        const begin = Offset(1.0, 0.0);
+                                        const end = Offset.zero;
+                                        const curve = Curves.easeInOut;
+
+                                        var tween = Tween(
+                                                begin: begin, end: end)
+                                            .chain(CurveTween(curve: curve));
+                                        var offsetAnimation =
+                                            animation1.drive(tween);
+
+                                        return SlideTransition(
+                                          position: offsetAnimation,
+                                          child: child,
+                                        );
+                                      },
+                                      transitionDuration:
+                                          const Duration(milliseconds: 1500),
+                                    ),
+                                  );
+                                },
+                                icon: const Row(
+                                  children: [
+                                    SizedBox(width: 8),
+                                    Icon(FontAwesomeIcons.bookOpen,
+                                        color: Color(0xFF322E53)),
+                                    SizedBox(
+                                        width:
+                                            8), // Espaço entre o ícone e o texto
+                                  ],
+                                ),
+                                style: TextButton.styleFrom(
+                                  alignment: Alignment.centerLeft,
+                                  minimumSize: const Size(100, 50),
+                                ),
+                                label: const Text(
+                                  "Secagem e Armazenamento de Grãos de Trigo, Girassol e Amendoim",
+                                  style: TextStyle(
+                                    fontFamily: 'Merriweather',
+                                    fontSize: 12,
+                                    color: Color(0xFF322E53),
+                                  ),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Container(
+                              margin:
+                                  const EdgeInsets.only(left: 20, right: 20),
+                              decoration: BoxDecoration(
+                                color: const Color(0xFFF1C28E),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: TextButton.icon(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    PageRouteBuilder(
+                                      pageBuilder:
+                                          (context, animation1, animation2) =>
+                                              AmendoimList(),
+                                      transitionsBuilder: (context, animation1,
+                                          animation2, child) {
+                                        const begin = Offset(1.0, 0.0);
+                                        const end = Offset.zero;
+                                        const curve = Curves.easeInOut;
+
+                                        var tween = Tween(
+                                                begin: begin, end: end)
+                                            .chain(CurveTween(curve: curve));
+                                        var offsetAnimation =
+                                            animation1.drive(tween);
+
+                                        return SlideTransition(
+                                          position: offsetAnimation,
+                                          child: child,
+                                        );
+                                      },
+                                      transitionDuration:
+                                          const Duration(milliseconds: 1500),
+                                    ),
+                                  );
+                                },
+                                icon: const Row(
+                                  children: [
+                                    SizedBox(width: 8),
+                                    Icon(FontAwesomeIcons.bookOpen,
+                                        color: Color(0xFF322E53)),
+                                    SizedBox(
+                                        width:
+                                            8), // Espaço entre o ícone e o texto
+                                  ],
+                                ),
+                                style: TextButton.styleFrom(
+                                  alignment: Alignment.centerLeft,
+                                  minimumSize: const Size(100, 50),
+                                ),
+                                label: const Text(
+                                  "Agricultura de Precisão Aplicada a Produção de Trigo, Girassol e Amendoim",
+                                  style: TextStyle(
+                                    fontFamily: 'Merriweather',
+                                    fontSize: 12,
+                                    color: Color(0xFF322E53),
+                                  ),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Container(
+                              margin:
+                                  const EdgeInsets.only(left: 20, right: 20),
+                              decoration: BoxDecoration(
+                                color: const Color(0xFFF1C28E),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: TextButton.icon(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    PageRouteBuilder(
+                                      pageBuilder:
+                                          (context, animation1, animation2) =>
+                                              AmendoimList(),
+                                      transitionsBuilder: (context, animation1,
+                                          animation2, child) {
+                                        const begin = Offset(1.0, 0.0);
+                                        const end = Offset.zero;
+                                        const curve = Curves.easeInOut;
+
+                                        var tween = Tween(
+                                                begin: begin, end: end)
+                                            .chain(CurveTween(curve: curve));
+                                        var offsetAnimation =
+                                            animation1.drive(tween);
+
+                                        return SlideTransition(
+                                          position: offsetAnimation,
+                                          child: child,
+                                        );
+                                      },
+                                      transitionDuration:
+                                          const Duration(milliseconds: 1500),
+                                    ),
+                                  );
+                                },
+                                icon: const Row(
+                                  children: [
+                                    SizedBox(width: 8),
+                                    Icon(FontAwesomeIcons.bookOpen,
+                                        color: Color(0xFF322E53)),
+                                    SizedBox(
+                                        width:
+                                            8), // Espaço entre o ícone e o texto
+                                  ],
+                                ),
+                                style: TextButton.styleFrom(
+                                  alignment: Alignment.centerLeft,
+                                  minimumSize: const Size(100, 50),
+                                ),
+                                label: const Text(
+                                  "Aplicação de Radiação Ionizante na Análise e Produção de Sementes",
                                   style: TextStyle(
                                     fontFamily: 'Merriweather',
                                     fontSize: 12,
